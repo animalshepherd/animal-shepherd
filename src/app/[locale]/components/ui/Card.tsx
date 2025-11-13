042ba09af3a67b6bb7ee4525fe3f93a6c6e10589
jacobadam <jacobanevitt@gmail.com>
2025-11-13 15:23:09 -0600

    add resusable card component

---
+++ src/app/[locale]/components/ui/Card.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+
+interface AnimalCardProps {
+  image: string;
+  title: string;
+  description: string;
+}
+
+export const Card = ({ image, title, description }: AnimalCardProps) => {
+  return (
+    <div className="rounded-xl bg-white shadow-lg overflow-hidden text-center">
+      <div className="relative w-full h-56">
+        <Image src={image} alt={title} fill className="object-cover" />
+      </div>
+      <div className="w-full p-6">
+        <h5 className="text-xl font-semibold font-manrope text-gray-900 mb-4">
+          {title}
+        </h5>
+        <p className="text-sm font-medium text-gray-600">{description}</p>
+      </div>
+    </div>
+  );
+};
