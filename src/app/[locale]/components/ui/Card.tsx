b74fd57b4e1723c33e7248b9f20fb10fdeab3563
jacobadam <jacobanevitt@gmail.com>
2025-11-18 13:19:04 -0600

    remove title and description from card component

--- src/app/[locale]/components/ui/Card.tsx
+++ src/app/[locale]/components/ui/Card.tsx
@@ -4,21 +4,14 @@ import Image from "next/image";
 
 interface AnimalCardProps {
   image: string;
-  title: string;
-  description: string;
+  alt: string;
 }
 
-export const Card = ({ image, title, description }: AnimalCardProps) => {
+export const Card = ({ image, alt }: AnimalCardProps) => {
   return (
     <div className="rounded-xl bg-primary shadow-lg overflow-hidden text-center">
       <div className="relative w-full h-56">
-        <Image src={image} alt={title} fill className="object-cover" />
-      </div>
-      <div className="w-full p-6">
-        <h5 className="text-xl font-semibold font-manrope text-gray-900 mb-4">
-          {title}
-        </h5>
-        <p className="text-sm font-medium text-gray-600">{description}</p>
+        <Image src={image} alt={alt} fill className="object-cover" />
       </div>
     </div>
   );
