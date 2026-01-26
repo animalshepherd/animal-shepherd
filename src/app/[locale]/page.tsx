6d694457a1c52c0c80c3383e3eb3c48972abc92d
jacobadam <jacobanevitt@gmail.com>
2026-01-26 15:15:11 -0600

    remove loading eager

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -31,7 +31,6 @@ export default function HomePage() {
             fill
             fetchPriority="high"
             quality={50}
-            loading="eager"
             alt="Founder interacting with sanctuary dogs"
             className="object-cover"
           />
