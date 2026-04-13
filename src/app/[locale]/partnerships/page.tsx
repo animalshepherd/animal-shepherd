cf6f692c8c086f60a240d8e485d8733cfee534ff
jacobadam <jacobanevitt@gmail.com>
2026-04-13 23:27:23 +0100

    remove size

--- src/app/[locale]/partnerships/page.tsx
+++ src/app/[locale]/partnerships/page.tsx
@@ -109,7 +109,7 @@ export default function PartnershipsPage() {
             </div>
 
             <div className="flex justify-center items-center lg:justify-start space-y-4">
-              <Button size="md" href="https://www.instagram.com/p/CGKs9bGp8T-/">
+              <Button href="https://www.instagram.com/p/CGKs9bGp8T-/">
                 {t("Art.button")}
               </Button>
             </div>
